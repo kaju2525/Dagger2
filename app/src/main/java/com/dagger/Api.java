7051cb9c787000abb579037a2c47fe7e86ee1984
karun_grepix <karun@grepixit.com>
2018-05-07 16:39:40 +0530

    demo

---
+++ app/src/main/java/com/dagger/Api.java
@@ -0,0 +1,16 @@
+package com.dagger;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by Belal on 10/2/2017.
+ */
+
+public interface Api {
+
+    @GET("marvel")
+    Call<List<Hero>> getHeroes();
+}
---
+++ app/src/main/java/com/dagger/ApiComponent.java
@@ -0,0 +1,19 @@
+package com.dagger;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by Belal on 12/2/2017.
+ */
+
+
+
+
+
+@Singleton
+@Component(modules = {AppModule.class, ApiModule.class})
+public interface ApiComponent {
+    void inject(MainActivity activity);
+}
